5be852088587d5962ecca8be6939c04bd293d527
fikriahaekal14 <haekalahmadf@gmail.com>
2022-02-13 02:18:45 +0700

    update products.php & product-specific.php

---
+++ product_specific.php
@@ -0,0 +1,367 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Specific Product</title>
+  <meta content="" name="description">
+
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/dekostumo.png" rel="icon">
+  <link href="assets/img/dekostumo.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <style>
+    .sosial-media{
+      text-align:center;
+    }
+    .sosial-media a {
+      font-size:120px;
+      color:white;
+      transition:0.5s;
+    }
+
+    .sosial-media a:hover {
+      font-size:120px;
+      color:#444;
+      
+    }
+
+    .judul-list{
+      font-weight: 800;
+      color:#000;
+    }
+
+    .judul-list ul li{
+      font-weight: 500;
+      color:#000;
+    }
+
+    .judul-list ul li a{
+            color:#000;
+    }
+
+    .judul-list ul li a:hover{
+            color:#444;
+    }
+
+    .judul-list ul li a:active{
+            color:#444;
+    }
+
+  </style>
+
+  <!-- =======================================================
+  * Template Name: FlexStart - v1.7.0
+  * Template URL: https://bootstrapmade.com/flexstart-bootstrap-startup-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header fixed-top">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center">
+        <img src="assets/img/dekostumo.png" alt="">
+        <span>DEKOSTUMO</span>
+      </a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Our Products</a></li>
+          <li><a class="nav-link scrollto" href="#about">Help</a></li>
+
+            <li><a href="blog.html">ID</a></li>|<li ><a style = "padding:0" href="blog.html">ENG</a></li>
+   
+          <!-- <li><a class="getstarted scrollto" href="#about">Get Started</a></li> -->
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="hero d-flex align-items-center" style = "margin-top:20px;">
+
+
+    <div class="container" style = "margin-top:1%;">
+      <!-- <center>
+        <h1>Our Products !</h1>
+      </center> -->
+   
+  
+  <br>
+  
+      <div class="row" style = "margin-bottom:20%;" >
+
+      
+      <?php
+      
+      $namaserver = 'localhost';
+      $username = 'root';
+      $password = '';
+      $DB = 'dekostumoid';
+    
+      $koneksi = new mysqli($namaserver, $username, $password, $DB);
+    
+      if($koneksi->connect_error)
+      {
+          die("Connection Error" . $koneksi->connect_error);
+      }
+
+      $product_id = trim(mysqli_real_escape_string($koneksi, $_GET['id'])); 
+    
+      $select_product_from_id = "SELECT * FROM products WHERE id = '" . $product_id . "' ";
+      $hasil_select_id = $koneksi->query($select_product_from_id);
+      
+
+      while($baris_id = mysqli_fetch_assoc($hasil_select_id))
+      {
+
+
+        $product_image_id = "./assets/img/baju/" . $baris_id['product_image'];
+        $gambar_satu_id = "./assets/img/baju/" . $baris_id['gambar1'];
+        $gambar_dua_id = "./assets/img/baju/" . $baris_id['gambar1'];
+        $gambar_tiga_id = "./assets/img/baju/" . $baris_id['gambar1'];
+      
+      
+      ?>
+
+
+
+
+        <div class="col-lg-8 d-flex flex-column justify-content-center" data-aos="zoom-out" data-aos-delay="200">
+         <img src="<?php echo $product_image_id  ?>" alt="">
+         <br>
+         <center>
+          <ul style = "list-style-type: none; overflow:hidden">
+            <li style = "float:left"><img src="<?php echo $product_image_id  ?>" alt="" style = "width:100px;margin-left: 15px; margin-right:15px;"></li>
+            <li style = "float:left"><img src="<?php echo $gambar_satu_id  ?>" alt="" style = "width:100px;margin-left: 15px; margin-right:15px;"></li>
+            <li style = "float:left"><img src="<?php echo $gambar_dua_id  ?>" alt="" style = "width:100px;margin-left: 15px; margin-right:15px;"></li>
+            <li style = "float:left"><img src="<?php echo $gambar_tiga_id  ?>" alt="" style = "width:100px;margin-left: 15px; margin-right:15px;"></li>
+            <!-- <li style = "float:left"><img src="./assets/img/baju/yellow-shirt.jpg" alt="" style = "width:100px;margin-left: 15px; margin-right:15px;"></li> -->
+        </ul>
+         </center>
+
+
+         
+        </div>
+
+
+
+        <div class="col-lg-4 d-flex flex-column justify-content-center" data-aos="zoom-out" data-aos-delay="200">
+          <!-- <img src="assets/img/heros.png" class="img-fluid" alt="" style = "margin-top:90px;"> -->
+          <div style = "text-align:right">
+            <h1><?php echo $baris_id['product_name'];  ?></h1>
+            <h2>IDR <?php echo $baris_id['product_price'];  ?></h2>
+            <br>
+            <h4><?php echo $baris_id['description'];  ?></h4>
+            <br>
+
+            <h2> <strong>Order Now</strong> </h2>
+            <ul style = "list-style-type: none; overflow:hidden; margin-top:10px;">
+              <li style = "float:left"><img src="<?php echo $gambar_satu_id  ?>" alt="" style = "width:85px;margin-left: 10px; margin-right:10px;"></li>
+              <li style = "float:left"><img src="<?php echo $gambar_dua_id  ?>" alt="" style = "width:85px;margin-left: 10px; margin-right:10px;"></li>
+              <li style = "float:left"><img src="<?php echo $gambar_tiga_id  ?>" alt="" style = "width:85px;margin-left: 10px; margin-right:10px;"></li>
+            
+          </ul>
+  
+          </div>
+
+
+        </div>
+        
+
+
+        <div class="col-lg-8" data-aos="zoom-out" data-aos-delay="200">
+        
+          <h2 style = "margin-bottom:10px;" > <strong>Description</strong> </h2>
+          <h6><?php echo $baris_id['description'];  ?></h6>
+
+          <br>
+
+          <h2 style = "margin-bottom:10px;" > <strong>Materials</strong> </h2>
+          <h6><?php echo $baris_id['materials'];  ?></h6> 
+          
+        </div>
+
+
+        <div class="col-lg-4" data-aos="zoom-out" data-aos-delay="200" style = "text-align:right;">
+        
+          <h5> <strong>Color</strong> </h5>
+          <ul style = "list-style-type: none; overflow:hidden; margin-top:10px;">
+            <li style = "float:right ; width:75px; height:75px; margin-left: 10px; margin-top:15px; margin-right:10px; background-color:red; border: 1px solid red ;"> </li>
+            <li style = "float:right ; width:75px; height:75px;margin-left: 10px; margin-top:15px; margin-right:10px; background-color:red; border: 1px solid red ; "> </li>
+            <li style = "float:right ; width:75px; height:75px;margin-left: 10px; margin-top:15px; margin-right:10px; background-color:red; border: 1px solid red ; "> </li>
+            <li style = "float:right ; width:75px; height:75px;margin-left: 10px;  margin-top:15px;margin-right:10px; background-color:red; border: 1px solid red ; "> </li>
+            <!-- <li style = "float:right"><div  style = "width:85px;margin-left: 10px; margin-right:10px; background-color:red " ></div></li>
+            <li style = "float:right"><div  style = "width:85px;margin-left: 10px; margin-right:10px; background-color:red " ></div></li> -->
+          
+        </ul>
+
+        <br>
+        <br>
+
+
+        <h5> <strong>Size</strong> </h5>
+        <ul style = "list-style-type: none; overflow:hidden; margin-top:10px;">
+          <li class="align-middle"  style = "float:right ; width:75px; height:75px; margin-left: 10px; margin-top:15px; margin-right:10px; background-color:#F4F4F4; border: 1px solid #F4F4F4 ; text-align:center;padding-top:20px;"> <span class="align-middle" style = "font-size:20px;">S</span> </li>
+          <li  style = "float:right ; width:75px; height:75px;margin-left: 10px; margin-top:15px; margin-right:10px; background-color:#F4F4F4; border: 1px solid #F4F4F4 ; text-align:center;padding-top:20px;"> <span class="align-middle" style = "font-size:20px;">M</span> </li>
+          <li style = "float:right ; width:75px; height:75px;margin-left: 10px; margin-top:15px; margin-right:10px; background-color:#F4F4F4; border: 1px solid #F4F4F4 ; text-align:center;padding-top:20px;"> <span class="align-middle" style = "font-size:20px;">L</span> </li>
+          <li style = "float:right ; width:75px; height:75px;margin-left: 10px;  margin-top:15px;margin-right:10px; background-color:#F4F4F4; border: 1px solid #F4F4F4 ; text-align:center;padding-top:20px;"> <span class="align-middle" style = "font-size:20px;">XLs</span></li>
+          <!-- <li style = "float:right"><div  style = "width:85px;margin-left: 10px; margin-right:10px; background-color:red " ></div></li>
+          <li style = "float:right"><div  style = "width:85px;margin-left: 10px; margin-right:10px; background-color:red " ></div></li> -->
+        
+      </ul>
+
+
+        </div>
+
+
+
+
+
+
+
+
+      </div>
+
+      <?php 
+      
+      }
+      ?>
+
+
+
+      <!-- <div class = "row">
+        <div class="col-lg-8" data-aos="zoom-out" data-aos-delay="200">
+        
+          <h2> <strong>Description</strong> </h2>
+          
+        </div>
+
+
+
+      </div> -->
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+
+
+
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer" style = "background-color: #fff; ">
+
+  
+    <center>
+    <div class="footer-top">
+
+      <div class="container" style = "text-align:center;">
+
+        <div class="row" style = "text-align:center;">
+          <div class="col-lg-3 col-6 footer-links">
+           <a href="" class = "align-items-center">
+            <h4>Terms & Conditions</h4>
+          </a>
+          </div>
+
+          <div class="col-lg-3 col-6 footer-links">
+            <a href="" class = "align-items-center">
+              <h4>Privacy Policy</h4>
+            </a>
+          </div>
+
+          <div class="col-lg-3 col-6 footer-links">
+            <a href="" class = "align-items-center">
+              <h4>About Us</h4>
+            </a>
+          </div>
+
+          <div class="col-lg-3 col-6 footer-links">
+            <a href="" class = "align-items-center">
+              <h4>Frequently Asked Question</h4>
+            </a>
+          </div>
+
+        </div>
+          </div>
+     
+
+    </div>
+  </center>
+
+
+  <center>
+
+    <div class="container" >
+      <div class="copyright">
+
+         
+            <img src="assets/img/dekostumo-logo-1.png" alt="" style = "width:40%;">
+             <!-- <strong><span style = "color:#050404; font-size: 20px;">DEKOSTUMO</span></strong> -->
+    
+
+       
+      </div>
+
+     </div>
+    </center>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
---
+++ products.php
@@ -0,0 +1,456 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Our Products</title>
+  <meta content="" name="description">
+
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/dekostumo.png" rel="icon">
+  <link href="assets/img/dekostumo.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <style>
+    .sosial-media{
+      text-align:center;
+    }
+    .sosial-media a {
+      font-size:120px;
+      color:white;
+      transition:0.5s;
+    }
+
+    .sosial-media a:hover {
+      font-size:120px;
+      color:#444;
+      
+    }
+
+    .judul-list{
+      font-weight: 800;
+      color:#000;
+    }
+
+    .judul-list ul li{
+      font-weight: 500;
+      color:#000;
+    }
+
+    .judul-list ul li a{
+            color:#000;
+    }
+
+    .judul-list ul li a:hover{
+            color:#444;
+    }
+
+    .judul-list ul li a:active{
+            color:#444;
+    }
+
+  </style>
+
+  <!-- =======================================================
+  * Template Name: FlexStart - v1.7.0
+  * Template URL: https://bootstrapmade.com/flexstart-bootstrap-startup-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="header fixed-top">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+      <a href="index.html" class="logo d-flex align-items-center">
+        <img src="assets/img/dekostumo.png" alt="">
+        <span>DEKOSTUMO</span>
+      </a>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Our Products</a></li>
+          <li><a class="nav-link scrollto" href="#about">Help</a></li>
+
+            <li><a href="blog.html">ID</a></li>|<li ><a style = "padding:0" href="blog.html">ENG</a></li>
+   
+          <!-- <li><a class="getstarted scrollto" href="#about">Get Started</a></li> -->
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <?php
+  
+  $namaserver = 'localhost';
+  $username = 'root';
+  $password = '';
+  $DB = 'dekostumoid';
+
+  $koneksi = new mysqli($namaserver, $username, $password, $DB);
+
+  if($koneksi->connect_error)
+  {
+      die("Connection Error" . $koneksi->connect_error);
+  }
+
+  $select_product = "SELECT * FROM products";
+  $hasil_select = $koneksi->query($select_product);
+
+ 
+  
+  ?>
+
+
+
+
+
+
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="hero d-flex align-items-center" style = "margin-top:50px;">
+
+
+    <div class="container" style = "margin-top:3%;">
+      <center>
+        <h1>Our Products !</h1>
+      </center>
+
+      <?php 
+      
+      // print_r($hasil_select);
+
+      // if(mysqli_num_rows($hasil_select) > 0)
+      // {
+      //   while($row = mysqli_fetch_assoc($hasil_select))
+      //   {
+      //     print_r($row);
+      //   }
+      // }
+
+
+      ?>
+   
+  
+  <br>
+  
+      <div class="row" style = "margin-bottom:20%;" >
+        <div class="col-lg-2 d-flex flex-column justify-content-center">
+         
+          <div data-aos="fade-up" data-aos-delay="600">
+            <div class="text-center text-lg-start">
+
+              <ul style = "list-style: none;">
+                <li class = "judul-list">Partners
+                  <ul style = "text-align:left">
+                    <li> <a href="">ABC</a> </li>
+                    <li><a href="">DEF</a></li>
+                    <li><a href="">GHI</a></li>
+                  </ul>
+                </li>
+                
+                <li></li>
+                <li></li>
+              </ul>
+
+
+
+              <ul style = "list-style: none;">
+                <li class = "judul-list">Tops
+                  <ul style = "text-align:left">
+                    <li> <a href="">ABC</a> </li>
+                    <li><a href="">DEF</a></li>
+                    <li><a href="">GHI</a></li>
+                  </ul>
+                </li>
+                
+                <li></li>
+                <li></li>
+              </ul>
+
+
+
+
+
+
+
+              <ul style = "list-style: none;">
+                <li class = "judul-list">Bottom
+                  <ul style = "text-align:left">
+                    <li> <a href="">ABC</a> </li>
+                    <li><a href="">DEF</a></li>
+                    <li><a href="">GHI</a></li>
+                  </ul>
+                </li>
+                
+                <li></li>
+                <li></li>
+              </ul>
+
+
+
+
+
+
+              <ul style = "list-style: none;">
+                <li class = "judul-list">Outwear
+                  <ul style = "text-align:left">
+                    <li> <a href="">ABC</a> </li>
+                    <li><a href="">DEF</a></li>
+                    <li><a href="">GHI</a></li>
+                  </ul>
+                </li>
+                
+                <li></li>
+                <li></li>
+              </ul>
+
+
+
+
+
+
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-10" data-aos="zoom-out" data-aos-delay="200">
+          <!-- <img src="assets/img/heros.png" class="img-fluid" alt="" style = "margin-top:90px;"> -->
+         
+
+
+          <?php 
+          // $baris = mysqli_fetch_assoc($hasil_select);
+
+          $array_kosong = array();
+
+ 
+            while($baris = mysqli_fetch_assoc($hasil_select))
+            {
+    
+    
+              
+
+          // print_r($baris);
+
+        
+          // foreach($array_kosong as $row)
+          // {
+
+
+            // while($baris)
+            // {
+      
+           $product_image = "./assets/img/baju/". $baris['product_image'];
+           $url_id = "http://localhost/alif/product_specific.php?id=" . $baris['id'];
+
+          
+          ?>
+         
+         <center>
+          <div class="col-5 produk" data-aos="zoom-out" data-aos-delay="200" style = "margin-left:10px; margin-right:10px;">
+            <!-- <img src="assets/img/heros.png" class="img-fluid" alt="" style = "margin-top:90px;"> -->
+          
+
+            <div class="card">
+              <center>
+                <img class="card-img-top" src="<?php echo $product_image;  ?>" alt="Card image cap">
+                <div class="card-body">
+
+                  <h6 class="card-title"> New </h6>
+                  <h5 class="card-title"> <strong> <?php echo  $baris['product_name'];  ?> </strong> </h5>
+                  <p class="card-text"> <strong><?php echo "IDR " . $baris['product_price'];  ?></strong> </p>
+                  <a href="<?php echo $url_id ?>" class="btn btn-primary">Lihat Produk</a>
+                </div>
+              </center>
+            </div>
+
+
+            
+          </div>
+
+        </center>
+
+
+
+
+        <?php
+              // }
+
+               
+            }
+    
+            mysqli_close($koneksi);
+        ?>
+
+
+
+
+
+          
+
+
+
+          <!-- <center>
+
+         
+          <div class="col-5 produk" data-aos="zoom-out" data-aos-delay="200" style = "margin-left:10px; margin-right:10px;">
+          
+        
+        
+
+
+            <div class="card" >
+
+              <center>
+                <img class="card-img-top" src="./assets/img/baju/yellow-shirt.jpg" alt="Card image cap">
+                <div class="card-body">
+                  <h6 class="card-title"> New </h6>
+                  <h5 class="card-title"> <strong>Yellow Shirt</strong> </h5>
+                  <p class="card-text"> <strong>IDR 99.999,00</strong> </p>
+                  <a href="#" class="btn btn-primary">Lihat Produk</a>
+                </div>
+              </center>
+
+
+            </div>
+        
+        
+        
+        
+          </div>
+        </center> -->
+
+
+
+
+          <br>
+          <!-- <center>
+            <a href="#" class="btn btn-dark" style = "margin-top:150px;">View More</a>
+          </center> -->
+          
+       
+        </div>
+        
+
+        <center>
+          <a href="#" class="btn btn-dark" style = "margin-top:150px;">View More</a>
+        </center>
+
+        
+
+
+
+
+
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+
+
+
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer" class="footer" style = "background-color: #fff; ">
+
+  
+    <center>
+    <div class="footer-top">
+
+      <div class="container" style = "text-align:center;">
+
+        <div class="row" style = "text-align:center;">
+          <div class="col-lg-3 col-6 footer-links">
+           <a href="" class = "align-items-center">
+            <h4>Terms & Conditions</h4>
+          </a>
+          </div>
+
+          <div class="col-lg-3 col-6 footer-links">
+            <a href="" class = "align-items-center">
+              <h4>Privacy Policy</h4>
+            </a>
+          </div>
+
+          <div class="col-lg-3 col-6 footer-links">
+            <a href="" class = "align-items-center">
+              <h4>About Us</h4>
+            </a>
+          </div>
+
+          <div class="col-lg-3 col-6 footer-links">
+            <a href="" class = "align-items-center">
+              <h4>Frequently Asked Question</h4>
+            </a>
+          </div>
+
+        </div>
+          </div>
+     
+
+    </div>
+  </center>
+
+
+  <center>
+
+    <div class="container" >
+      <div class="copyright">
+
+         
+            <img src="assets/img/dekostumo-logo-1.png" alt="" style = "width:40%;">
+             <!-- <strong><span style = "color:#050404; font-size: 20px;">DEKOSTUMO</span></strong> -->
+    
+
+       
+      </div>
+
+     </div>
+    </center>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
